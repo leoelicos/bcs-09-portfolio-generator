c419560746d51ee297ceac412fae576f006fb9ab
Leo Wong <leoelicos@gmail.com>
2022-05-25 17:13:25 +1000

    Change require to ES6 import, implement async syntax

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-const inquirer = require('inquirer');
-const fs = require('fs');
+import chalk from 'chalk';
+import inquirer from 'inquirer';
+import { writeFile } from 'fs';
 
 const generateHTML = ({ name, location, github, linkedin }) =>
 	`<!DOCTYPE html>
@@ -8,58 +9,97 @@ const generateHTML = ({ name, location, github, linkedin }) =>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-  <title>Document</title>
+  <title>${name}'s Portfolio'</title>
 </head>
 <body>
   <div class="jumbotron jumbotron-fluid">
   <div class="container">
     <h1 class="display-4">Hi! My name is ${name}</h1>
     <p class="lead">I am from ${location}.</p>
-    <h3>Example heading <span class="badge badge-secondary">Contact Me</span></h3>
+    <h3>Portfolio <span class="badge badge-secondary">Contact Me Here:</span></h3>
     <ul class="list-group">
-      <li class="list-group-item">My GitHub username is ${github}</li>
-      <li class="list-group-item">LinkedIn: ${linkedin}</li>
+      <li class="list-group-item">My GitHub username is <a href="https://github.com/${github}">${github}</a></li>
+      <li class="list-group-item">My LinkedIn is <a href="https://www.linkedin.com/in/${linkedin}">${linkedin}</a></li>
     </ul>
   </div>
 </div>
 </body>
 </html>`;
 
-inquirer
-	.prompt([
-		{
-			type: 'input',
-			name: 'name',
-			message: 'What is your name?'
-		},
-		{
-			type: 'input',
-			name: 'location',
-			message: 'Where are you from?'
-		},
-		{
-			type: 'input',
-			name: 'hobby',
-			message: 'What is your favorite hobby?'
-		},
-		{
-			type: 'input',
-			name: 'food',
-			message: 'What is your favorite food?'
-		},
-		{
-			type: 'input',
-			name: 'github',
-			message: 'Enter your GitHub Username'
-		},
-		{
-			type: 'input',
-			name: 'linkedin',
-			message: 'Enter your LinkedIn URL.'
-		}
-	])
-	.then((answers) => {
+const questions = [
+	{
+		type: 'input',
+		name: 'name',
+		prefix: ' What is your',
+		message: chalk.rgb(255, 154, 162)('name'),
+		suffix: '?'
+	},
+	{
+		type: 'input',
+		name: 'location',
+		prefix: ' What is your',
+		message: chalk.rgb(255, 183, 178)('location'),
+		suffix: '?'
+	},
+	{
+		type: 'input',
+		name: 'hobby',
+		prefix: ' What is your',
+		message: chalk.rgb(255, 218, 193)('favorite hobby'),
+		suffix: '?'
+	},
+	{
+		type: 'input',
+		name: 'food',
+		prefix: ' What is your',
+		message: chalk.rgb(226, 240, 203)('favorite food'),
+		suffix: '?'
+	},
+	{
+		type: 'input',
+		name: 'github',
+		prefix: ' What is your',
+		message: chalk.rgb(181, 234, 215)('GitHub username'),
+		suffix: '?'
+	},
+	{
+		type: 'input',
+		name: 'linkedin',
+		prefix: ' What is your',
+		message: chalk.rgb(199, 206, 234)('LinkedIn URL'),
+		suffix: '?'
+	}
+];
+
+// helper function to pad messages
+const screenWidth = 80;
+const pad = (text) => text.padEnd(screenWidth);
+const init = async () => {
+	const welcomeMessage = chalk.black.bgWhite(pad('') + '\n' + pad('PORTFOLIO GENERATOR') + '\n' + pad(''));
+	console.log(welcomeMessage);
+
+	try {
+		const answers = await inquirer.prompt(questions);
+
 		const htmlPageContent = generateHTML(answers);
 
-		fs.writeFile('index.html', htmlPageContent, (err) => (err ? console.log(err) : console.log('Successfully created index.html!')));
-	});
+		const writeResponse = await new Promise((resolve, reject) =>
+			writeFile('./dist/index.html', htmlPageContent, (err) => {
+				if (err) {
+					reject(new Error(err));
+				}
+				const message = chalk.rgb(255, 154, 162)('Successfully created index.html in the dist/ folder!');
+				resolve(message);
+			})
+		);
+
+		console.log(writeResponse);
+	} catch (err) {
+		console.error(err);
+	}
+
+	const goodbyeMessage = chalk.black.bgWhite(pad('') + '\n' + pad('Thank you for using Portfolio Generator.') + '\n' + pad(''));
+	console.log(goodbyeMessage);
+};
+
+init();
